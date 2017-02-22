b1be8016c5ef4e47b6127f83faba2490b9983754
chris <c.giacofei@gmail.com>
2017-02-22 10:50:17 -0500

    Rename files with when moving them.

--- photo.sh
+++ photo.sh
@@ -6,6 +6,6 @@ CARRIE_INPUT=$HOME/files/photos/carrie-camera
 
 OUTPUT_DIR=$HOME/files/photos/combined
 
-$EXE -d $OUTPUT_DIR/%Y/%m "-directory<CreateDate" $CHRIS_INPUT
-$EXE -d $OUTPUT_DIR/%Y/%m "-directory<CreateDate" $CARRIE_INPUT
+$EXE -r -d $OUTPUT_DIR/%Y/%m/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CHRIS_INPUT
+$EXE -r -d $OUTPUT_DIR/%Y/%m/%Y-%m-%d_%H%M%S%%-c.%%e  "-filename<CreateDate" "-filename<DateTimeOriginal" $CARRIE_INPUT
 
