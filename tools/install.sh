afb08b773f6d3230e522bfe52d8146092f5c1c22
chris <c.giacofei@gmail.com>
2017-02-22 13:23:30 -0500

    witch to directory first.

--- tools/install.sh
+++ tools/install.sh
@@ -5,11 +5,11 @@ if git clone --recursive --depth=1 https://github.com/cgiacofei/scripts.git $HOM
    echo Repository successfully cloned. 
 fi
 
+cd $HOME/.scripts
 STATUS=$HOME/.scripts/tools/check_repo
 
 if [[ $STATUS == 1 ]] ; then
     sh $HOME/.scripts/tools/upgrade.sh
-    cd $HOME/.scripts
     git submodule sync --recursive
     sh $HOME/.scripts/tools/linkfiles.sh
 fi
