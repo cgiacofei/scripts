b89e8363e041672b1d980c2330e1c0b4ae6b3f6d
chris <c.giacofei@gmail.com>
2017-02-22 08:17:49 -0500

    Modify install script.
    If can't be cloned (already exists) then run upgrade script followed
    by linking executables.

--- tools/install.sh
+++ tools/install.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 
+BIN="rsync_backup ghi/ghi"
+
 # Clone script repo into temp directory
-if git clone --recursive --depth=1 https://github.com/cgiacofei/scripts.git $HOME/.scripts ; then
-
-        BIN="todo/todo.sh rsync_backup ghi/ghi"
-    TEST=$1
-
-    for exe in $BIN; do
-        echo Will run: ln -s  $HOME/.scripts/$exe $HOME/bin/$(basename $exe)
-    done
-else
-    echo Already installed update instead
+if ! git clone --recursive --depth=1 https://github.com/cgiacofei/scripts.git $HOME/.scripts ; then
+    echo Already installed updating.
     sh $HOME/.scripts/tools/upgrade.sh
 fi
 
+for exe in $BIN; do
+    echo Will run: ln -s  $HOME/.scripts/$exe $HOME/bin/$(basename $exe)
+done
