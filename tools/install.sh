b2794ef7f696147be62a1e5ee7c3d1dfc0a2e679
chris <c.giacofei@gmail.com>
2017-02-23 14:52:00 -0500

    Remove repo checking

--- tools/install.sh
+++ tools/install.sh
@@ -8,10 +8,7 @@ if git clone --recursive --depth=1 https://github.com/cgiacofei/scripts.git $HOM
 fi
 
 cd $HOME/.scripts
-STATUS=$(sh $HOME/.scripts/tools/check_repo)
-echo $STATUS
-if [[ $STATUS == 1 ]] ; then
-    sh $HOME/.scripts/tools/upgrade.sh
-    git submodule sync --recursive
-    sh $HOME/.scripts/tools/linkfiles.sh
-fi
+
+sh $HOME/.scripts/tools/upgrade.sh
+git submodule sync --recursive
+sh $HOME/.scripts/tools/linkfiles.sh
