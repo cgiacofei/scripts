9a45a40ea8f4d06eeac373d8b3fe858ccbd9d35b
chris <c.giacofei@gmail.com>
2017-02-23 15:34:44 -0500

    Home directory links for extra stuff.

--- tools/linkfiles.sh
+++ tools/linkfiles.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
 BIN="app_check rsync_backup ghi/ghi binary/ncdu photo.sh"
+EXTRA="todo/.todo.actions.d"
 
 echo --- Linking Scripts ---
 for exe in $BIN; do
     echo "Linking $HOME/.scripts/$exe --> $HOME/bin/$(basename $exe)"
     ln -sf  $HOME/.scripts/$exe $HOME/bin/$(basename $exe)
+done
+
+echo --- Linking Extra Files ---
+for exe in $EXTRA; do
+    echo "Linking $HOME/.scripts/$exe --> $HOME/$(basename $exe)"
+    ln -sf  $HOME/.scripts/$exe $HOME/$(basename $exe)
 done
